4ed5cc7c163fa1674a99b01c5019197517c714e0
AlbertoHuizar <huizar.blanco@gmail.com>
2025-11-05 16:32:52 -0600

    solution

--- game.js
+++ game.js
@@ -9,4 +9,41 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let choice = prompt("Enter rock, paper, or scissors:");
+    return choice.toLowerCase();
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("Empate!");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`Ganaste! ${humanChoice} vence a ${computerChoice}`);
+    humanScore++;
+  } else {
+    console.log(`Perdiste! ${computerChoice} vence a ${humanChoice}`);
+    computerScore++;
+  }
+}
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+
+  console.log("Puntaje final:");
+  console.log("Humano:", humanScore);
+  console.log("Computadora:", computerScore);
+}
+
+playGame();
